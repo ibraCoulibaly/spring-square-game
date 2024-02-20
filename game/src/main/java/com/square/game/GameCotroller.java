ea162d202f75237df2aca866e39f59d3caaad60c
Ibrahima COULIBALY <ibrahima.coulibaly@Digital-Grenoble.local>
2024-02-20 12:11:52 +0100

    méthode POST pour créer une partie tictactoe en passans par GameDTO

--- game/src/main/java/com/square/game/GameCotroller.java
+++ game/src/main/java/com/square/game/GameCotroller.java
@@ -1,27 +1,20 @@
 package com.square.game;
 
-import fr.le_campus_numerique.square_games.engine.Game;
 import fr.le_campus_numerique.square_games.engine.GameFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.*;
-
 @RestController
 
 public class GameCotroller {
     @Autowired
     private GameCatalog gameCatalog;
-
-    private GameFactory gameFactory;
-
-
     @PostMapping("/games")
-        public Game createGame(@RequestBody GameCreationParams params){
-        gameFactory = gameCatalog.getFactoryById(params.getTypeGame());
-        return gameFactory.createGame(params.getPlayerCount(), params.getBoardSize());
-        //return UUID.randomUUID().toString();
+        public GameDTO createGame(@RequestBody GameCreationParams params){
+            GameFactory gameFactory = gameCatalog.getFactoryById(params.getTypeGame());
+            gameFactory.createGame(params.getPlayerCount(), params.getBoardSize());
+            return new GameDTO(gameFactory.getGameId(), params.getPlayerCount(), params.getBoardSize());
     }
 }
--- game/src/main/java/com/square/game/GameDTO.java
+++ game/src/main/java/com/square/game/GameDTO.java
@@ -1,17 +1,8 @@
 package com.square.game;
 
-public class GameDTO {
-    private String id;
-
-    public GameDTO(String id){
-        this.id = id;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
+public record GameDTO(
+         String gameType,
+         int playerCount,
+         int boardSize
+) {
 }
