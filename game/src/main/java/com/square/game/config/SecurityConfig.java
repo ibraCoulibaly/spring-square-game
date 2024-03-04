0dc442e0f7dd7c463b389ee9e0c49455e01eb45e
Ibrahima Coulibaly <coulibra4@gmail.com>
2024-03-04 08:26:06 +0100

    ajout de l'objet UserDetails pour donner un role aux utilisateurs

--- game/src/main/java/com/square/game/config/SecurityConfig.java
+++ game/src/main/java/com/square/game/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -18,10 +20,34 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((auth)->auth
-                        .requestMatchers("/ad/Produits").authenticated().anyRequest().permitAll()
+                        .requestMatchers("/ProduitsList").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers("/ad/Produits").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers("/up/Produits").hasRole("ADMIN")
+                        .requestMatchers("/sup/Produits/{id}").hasRole("ADMIN")
                 ).sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
 
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("user"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new  InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
